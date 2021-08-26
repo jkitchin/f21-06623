4533097555f8d278edd809e4eeb25561576de261
John Kitchin <jkitchin@andrew.cmu.edu>
2021-08-26 17:09:03 -0400

    rm URLs where possible

--- f21_06623/01_jupyter_coding.py
+++ f21_06623/01_jupyter_coding.py
@@ -6,10 +6,7 @@
 # In[1]:
 
 
-from urllib.request import urlopen
-url = 'https://drive.google.com/uc?id=1vZZBZ9UOBhjMSAw_viB6m4x5R6I52zYa'
-py = urlopen(url).read().decode('utf-8')
-exec(py)
+from MCQs import *
 
 
 # # Coding
@@ -33,10 +30,10 @@ import matplotlib.pyplot as plt
 def strip_magic(line, cell):
     lines = cell.split('\n')
     stripped_lines = [line for line in lines if not line.strip().startswith('%')]
-    
+
     if(len(lines)>len(stripped_lines)):
         print('Warning: The % magic does not work in this cell.')
-    
+
     return ('\n'.join(stripped_lines))
 
 
@@ -44,7 +41,7 @@ def strip_magic(line, cell):
 
 
 def create_new_cell(contents):
-    
+
     shell = get_ipython()
     shell.set_next_input(contents, replace=False)
 
@@ -66,23 +63,23 @@ def within_num(x, y):
 
 @register_cell_magic
 def L01Q1(line, cell):
-    
-    # correct answer 
+
+    # correct answer
     def correct(x):
         import numpy as np
         return np.sqrt(x)
-    
+
     globals = dict()
     exec(cell, globals)
-    
+
     # Now we can check if something was done
     sqrt = globals.get('square_root', None)
     n = globals.get('num', None)
-    
+
     if n is None:
         print('You did not define a "num" variable.')
         return
-    
+
     if sqrt is None:
         print('You did not define a "square_root" variable.')
     elif within_num(sqrt, correct(n)):
@@ -97,9 +94,9 @@ def L01Q1(line, cell):
 
 
 def Code1():
-    
+
     display(Markdown('Create a function that returns the square root of a number.'))
-    
+
     c = """%%L01Q1
 # import the required packages
 
@@ -107,12 +104,12 @@ def Code1():
 def f(x):
     ans =     # complete the line
     return ans
-    
+
 num =     # enter any number to find the square root
 square_root =      # call the function
 
 print(f'Your answer: {square_root}')"""
-    
+
     create_new_cell(c)
 
 
@@ -137,19 +134,19 @@ print('Code1() imported')
 
 @register_cell_magic
 def L01Q2(line, cell):
-    
-    # correct answer 
+
+    # correct answer
     def correct(x, y):
         return x+y
-    
+
     globals = dict()
     exec(cell, globals)
-    
+
     # Now we can check if something was done
     s1 = globals.get('string1', None)
     s2 = globals.get('string2', None)
     s = globals.get('string_', None)
-    
+
     if s1 is None:
         print('Looks like you have changed the "string1" variable. Use the original template variables.')
         return
@@ -159,7 +156,7 @@ def L01Q2(line, cell):
     if s is None:
         print('Looks like you have changed the "string_" variable. Use the original template variables.')
         return
-    
+
     if correct(s1, s2)==s:
         print('Correct')
     else:
@@ -172,19 +169,19 @@ def L01Q2(line, cell):
 
 
 def Code2():
-    
-    display(Markdown("""Create a program that takes 2 strings and returns a single string 
+
+    display(Markdown("""Create a program that takes 2 strings and returns a single string
     that has string 1 as the first half and string 2 as the later."""))
-    
+
     c = """%%L01Q2
 # define the two strings
-string1 = 
-string2 = 
+string1 =
+string2 =
 
 # combine the strings and assign to the variable 'string_'
 
 """
-    
+
     create_new_cell(c)
 
 
@@ -216,23 +213,23 @@ def within_array(x, y):
 
 @register_cell_magic
 def L01Q3(line, cell):
-    
-    # correct answer 
+
+    # correct answer
     def correct(n1, n2):
         q = n1//n2
         d = n1%n2
-    
+
         return q, d
-    
+
     globals = dict()
     exec(cell, globals)
-    
+
     # Now we can check if something was done
     n1 = globals.get('num1', None)
     n2 = globals.get('num2', None)
     Q = globals.get('Q', None)
     R = globals.get('R', None)
-    
+
     if n1 is None:
         print('Looks like you have changed the "num1" variable. Use the original template variables.')
         return
@@ -245,7 +242,7 @@ def L01Q3(line, cell):
     if R is None:
         print('Looks like you have changed the "R" variable. Use the original template variables.')
         return
-    
+
     if within_array(np.array([Q, R]), np.array(correct(n1, n2))):
         print('Correct')
     else:
@@ -258,21 +255,21 @@ def L01Q3(line, cell):
 
 
 def Code3():
-    
+
     display(Markdown("""Create a program that takes 2 numbers and returns
     the quotient and dividend of number1 divided by number2."""))
-    
+
     c = """%%L01Q3
 # define the two numbers
-num1 = 
-num2 = 
+num1 =
+num2 =
 
 # assign the quotient and remainder to the variables defined below
-Q = 
-R = 
+Q =
+R =
 
 """
-    
+
     create_new_cell(c)
 
 
@@ -286,4 +283,3 @@ R =
 
 
 print('Code3() imported')
-
--- f21_06623/MCQs.py
+++ f21_06623/MCQs.py
@@ -11,13 +11,17 @@ import numpy as np
 from IPython.core.getipython import get_ipython
 
 
-# google spreadsheet with all lectures. Contains fields: date, tag, lecture, question, answer. Below link is a modified url of the google spreadsheet. Need to keep sharing permission to 'anyone with the link can open'.
+# google spreadsheet with all lectures. Contains fields: date, tag, lecture,
+# question, answer. Below link is a modified url of the google spreadsheet. Need
+# to keep sharing permission to 'anyone with the link can open'.
 
 # In[2]:
 
 
 url = 'https://docs.google.com/spreadsheets/d/1qSaBe73Pd8L3jJyOL68klp6yRArW7Nce/export?format=xlsx&gid=1923176268'
 df = pd.read_excel(url)
+
+print(df)
 
 
 # Dictionary of lecture dates and names; Date is not currently used anywhere.
@@ -28,7 +32,7 @@ df = pd.read_excel(url)
 lectures = {'08/31/2020':'00_intro',
            '09/02/2020': '01_jupyter',
            '09/09/2020': '02_integration_1',
-           '09/14/2020': '03_fode_1', 
+           '09/14/2020': '03_fode_1',
             '09/16/2020': '04_fode_2',
             '09/21/2020': '05_nth_odes',
             '09/23/2020': '07_nla_1',
@@ -58,7 +62,7 @@ listOfLectures = list(lectures.values())
 
 
 def create_new_cell(contents):     # does not work in Colab
-    
+
     shell = get_ipython()
     shell.set_next_input(contents, replace=False)
 
@@ -67,10 +71,10 @@ def create_new_cell(contents):     # does not work in Colab
 
 
 def find_tags(keys):
-    
+
     # Empty dataframe to which tagged rows are added
     tdf = pd.DataFrame(columns = lec.columns)
-    
+
     for k in keys:     # for multiple tags as input
         for i in range(0, len(lec)):
             if(k in lec.iloc[i].tag):
@@ -83,9 +87,9 @@ def find_tags(keys):
 
 def reset():
     global old_q     # to record questions already answered for ensuring no repeats
-    old_q = []   
+    old_q = []
     global count     # to count the number of questions which were correctly answered
-    count = []  
+    count = []
     global current_tag   # to record a change in tag
     current_tag = ''
 reset()
@@ -94,7 +98,7 @@ reset()
 # In[7]:
 
 
-def TAG_DF(keys=[]):     # to handle multiple tags as input 
+def TAG_DF(keys=[]):     # to handle multiple tags as input
 
     global current_tag, count, lec
 
@@ -105,35 +109,35 @@ def TAG_DF(keys=[]):     # to handle multiple tags as input
         current_tag = keys
         check=0
         return None, check
-    
+
     if(set(keys).issubset(set(tags))==False or keys==[]):     # if tag is invalid
         print('Tags Invalid; Type MCQ([\'tags\']) to see all the available tags')
         check = 0
         return None, check
-    else:                          # if tag is valid   
-        
+    else:                          # if tag is valid
+
         # handling a change of tag, if requested before completing all the questions from previous tag
         if(set(keys).issubset(set(current_tag))==False and len(count)!=0 and current_tag!=['tags'] or len(current_tag)!=len(keys) and len(count)!=0):
-            print('There are unanswered questions for the last tag\s. Do you still want to change the tags? (y/n)')       
+            print('There are unanswered questions for the last tag\s. Do you still want to change the tags? (y/n)')
             change = input()
             if(change=='y'):
                 reset()
             else:
-                keys=current_tag   
-        
+                keys=current_tag
+
         if('all' in set(keys)):
             if(len(lec)>0):
                 tagged_df = lec
                 current_tag = keys
-            else: 
+            else:
                 print(f'Select a lecture from \n{listOfLectures} \n\nand run lecName([\'lecture_file_name\'])')
                 check=0
                 return None, check
-            
+
         else:
             tagged_df = find_tags(keys)     # data frame with just the tagged questions
             current_tag = keys
-            
+
     return tagged_df, check
 
 
@@ -150,27 +154,27 @@ def nameTags(l):
 # In[9]:
 
 
-def lecName(keys=None):   
-    
+def lecName(keys=None):
+
     """ Selects the lecture from which questions will be presented in this file.
-    
+
     Parameters:
     ___________
-    
+
     keys: (list of string/s): ['lecture_file_name/s'] or ['available'] or ['all']
-    
-    if ['lecture_file_name/s']: generates a dataframe only with the lectures included in the list 
+
+    if ['lecture_file_name/s']: generates a dataframe only with the lectures included in the list
                                 eg. lecName(['00_intro'];
     if ['available']: prints all the available lecture names;
     if ['all']: generates a dataframe with all the lectures.
     """
-    
+
     global lec
-    
+
     if(keys==['available']):
         print(f'Select a lecture from \n{listOfLectures} \n\nand run lecName([\'lecture_file_name\']).')
         return
-        
+
     if(keys==['all']):
         lec = df
         print(f'Imported MCQs for lecture {keys}.')
@@ -178,7 +182,7 @@ def lecName(keys=None):
 
     lec = df[df['lecture file'].isin(keys)]
     print(f'Imported MCQs for lecture {keys}.')
-    
+
     return # lec
 
 # default
@@ -191,59 +195,59 @@ lec = pd.DataFrame()
 
 
 def MCQ(keys=[]):
-    
+
     """Prints multiple choice questions based on the selected tags in the chosen lectures.
-    
+
     Parameters:
     ___________
-    
+
     keys: (list of string/s): ['tags'] or ['tag_name/s'] or ['all']
-    
+
     if ['tags']: prints the available tags;
     if ['tag_name/s']: presents questions only based on the selected tags;
                        eg. MCQ(['numpy', 'scipy'])
     if ['all']: presents questions for all the tags available from the selected lectures;
-    
+
     """
 
     global lec, tags, current_tag
-    
+
     # checks that a lecture has been selected, otherwise asks to do so
     if(len(lec)<1):
         print(f'Select a lecture from \n{listOfLectures} \n\nand run lecName([\'lecture_file_name\'])')
         return
-   
+
     # collects all the tags from the chosen lecture/s
     tags = nameTags(lec)
-    
+
     # dataframe with just tagged questions
     filtered_df, check = TAG_DF(keys)
     if(check==0):
         return
-   
+
     tagged_df = filtered_df
-    
+
     # randomizing the questions
     rand = list(np.random.choice(len(tagged_df), size = len(tagged_df), replace = False))
 
     # selecting just the unanswered questions
     next_q = [x for x in rand if x not in old_q]
-    
+
     # first question in the list p will be printed, saving it in old_q to avoid repeating in the next run
     old_q.append(next_q[0])
 
-    # printing 
+    # printing
     n = tagged_df.iloc[next_q[0]]
-    
+
     q = f'Q.({len(old_q)}/{len(tagged_df)})\n{n.question}\n'
     print(q)
-    
+
     options = f'a) {n.a}\nb) {n.b}\nc) {n.c}\nd) {n.d}\n'
     print(options)
-    
+
     # for answering multiple times until correct
     counter = 0
-    
+
     feed = input()
     while(counter==0):
         if(feed == n.answer):
@@ -266,4 +270,3 @@ def MCQ(keys=[]):
 
 
 # print('MCQ() imported')
-
